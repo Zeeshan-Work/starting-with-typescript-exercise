fb55a793acd6d0c2efcfa63f8e4816658b375007
Zeeshan-algo <muhammad.zeeshan@algo.com>
2022-03-14 14:13:19 -0400

    Merging sub branch to feature branch

--- src/BaseService.ts
+++ src/BaseService.ts
@@ -7,3 +7,19 @@ class BaseService{
 }
 
 export default BaseService
+
+
+
+
+class LoggingService extends BaseService {
+    logger: ILogger
+    logger = undefined
+    constructor(logger: ILogger) {
+        super()
+        if (logger === undefined || logger === null || Array.isArray(logger) || typeof logger !== 'object') {
+            throw new Error(`LoggingService: Invalid logger ${logger}`)
+        }
+
+        this.logger = logger
+    }
+}

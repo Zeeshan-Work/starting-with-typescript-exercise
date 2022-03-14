42caa500884c69811d77e4286a75bdb0e89eeb49
Zeeshan-algo <muhammad.zeeshan@algo.com>
2022-03-14 13:46:42 -0400

    Make changes in base service

--- src/BaseService.ts
+++ src/BaseService.ts
@@ -1,6 +1,12 @@
 class BaseService{
-    constructor(){
+    logger: ILogger
+    constructor(logger: ILogger) {
+        super()
+        if (logger === undefined || logger === null || Array.isArray(logger) || typeof logger !== 'object') {
+            throw new Error(`LoggingService: Invalid logger ${logger}`)
+        }
 
+        this.logger = logger
     }
 }
 
